f5988a979a26c915d21c46c2b9cfe512a994212e
Arif Balik <arifbalik@outlook.com>
2016-10-08 17:58:37 +0300

    Update zoetropefin_Ver1.ino

--- zoetropefin_Ver1.ino
+++ zoetropefin_Ver1.ino
@@ -21,11 +21,13 @@
 #include <Stepper.h>
 
 #define LED 13
-
 #define SENSOR1 8
 #define SENSOR2 9
 
-// revolution per animation (+1 for while loop)
+/* loop count per animation (+1 for while loop)
+ * There is 10 steps at 24RPM per loop cycle in Animation1
+ * and 18 steps at 44RPM per loop cycle in Animation2
+ */
 #define ANM1 (210 + 1)
 #define ANM2 (240 + 1)
 
@@ -34,26 +36,28 @@ Stepper stepper_motor(200, 4, 5, 6, 7);  // steps_per_rev, ain1_pin, ain2_pin, b
 void setup(){
   pinMode(LED, OUTPUT);
 
-  /*
-    There are two different Input source and both of them is PIR sensor from adafruit.
-  */
+  // There are two different Input source and both of them is PIR sensor from adafruit.
   pinMode(SENSOR1, INPUT);
   pinMode(SENSOR2, INPUT);
   
 }
 
+void setFrameVisible(int delayT){
+  digitalWrite(LED, HIGH);  
+  delay(delayT);    
+  digitalWrite(LED, LOW); 
+}
+
+void nextFrame(byte anmType){
+  stepper_motor.setSpeed((anmType == ANM1) ? 24 : 44); //RPM value 
+  stepper_motor.step((anmType == ANM1) ? 10 : 18);
+}
+
 void playAnimation(byte anmType){
   byte temp = anmType;
-  
-  stepper_motor.setSpeed((anmType == ANM1) ? 24 : 44); //RPM value 
- 
   while(--temp){
-    
-    digitalWrite(LED, HIGH);  
-    delay(5); //high interval ms      
-    digitalWrite(LED, LOW);  
-
-   stepper_motor.step((anmType == ANM1) ? 10 : 18);
+    setFrameVisible(5); 
+    nextFrame(anmTpye);
   }
 }
 
@@ -63,13 +67,11 @@ void loop(){
    *  number 1 and 2 will automatically play for 4 times
    */
   
-  short repeat_anm = 5;
-  
-  if(digitalRead(SENSOR1) || digitalRead(SENSOR2)){
-    while(--repeat_anm){
+  short repeat_anm = 4 + 1; // +1 for loop
+ 
+    while(--repeat_anm && (digitalRead(SENSOR1) || digitalRead(SENSOR2)) ){
      playAnimation(ANM1);
      playAnimation(ANM2);
     }
-  } 
+    
 }
-
